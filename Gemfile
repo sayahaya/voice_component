c4098258bef3af533299400e0e888e9153948170
sayahaya <sayahaya1129@gmail.com>
2021-06-15 10:31:55 +0900

    rspecを記載 #26

--- Gemfile
+++ Gemfile
@@ -37,5 +37,10 @@ group :development do
   gem 'rails_best_practices'
 end
 
+group :test do
+  gem 'capybara'
+  gem 'webdrivers'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ spec/system/top_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "TopPages", type: :system do
+  context '使用方法のページにアクセス' do
+    it '正しく表示される' do
+      visit root_path
+      click_on 'Voice componentとは'
+      expect(page).to have_content 'Voice componentとは'
+      expect(page).to have_selector("img[src$='recording-f03a3ed41c92298383dbb7c5d074e4344773c072210a52816637e884837598b0.png']")
+      expect(page).to have_selector("img[src$='result-7f6861cf8d69f8be7f734f9acda182af2dd6b3a8884b3a259a5b957779bd8f56.png']")
+      expect(page).to have_link 'トップページへ'
+    end
+  end
+
+  context '利用規約ページにアクセス' do
+    it '正しく表示される' do
+      visit root_path
+      click_on '利用規約'
+      expect(page).to have_content '利用規約'
+      expect(page).to have_link 'トップページへ'
+    end
+  end
+  
+  context 'プライバシーポリシーページにアクセス' do
+    it '正しく表示される' do
+      visit root_path
+      click_on 'プライバシーポリシー'
+      expect(page).to have_content 'プライバシーポリシー'
+      expect(page).to have_link 'トップページへ'
+    end
+  end
+
+  context 'お問い合わせ先ページへアクセス' do
+    it '正しく表示される' do
+      visit root_path
+      click_on 'お問い合わせ先'
+      expect(page).to have_content '@yama2_0506'
+      expect(page).to have_link 'トップページへ'
+    end
+  end
+end
